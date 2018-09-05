f7e9abe4629895f46e7a6e0f510c41431d47b3a9
malcolmhoward <malcolm.g.howard@gmail.com>
2018-09-05 01:44:09 -0400

    Added player score save file, text file handler script, and score save system script.

---
+++ Assets/Scripts/ScoreSaveSystemScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSaveSystemScript : MonoBehaviour {
+    public Button button_obj;
+    public string player_name;
+    public string player_score;
+    public Time timestamp;
+    public TextAsset save_file;
+    public TextFileHandler save_file_handler;
+
+	// Use this for initialization
+	void Start () {
+		player_name = GameObject.FindGameObjectsWithTag("PlayerName")[0].GetComponent<Text>().text;
+        Debug.Log(player_name);
+		player_score = GameObject.FindGameObjectsWithTag("PlayerScore")[0].GetComponent<Text>().text;
+        Debug.Log(player_score);
+        save_file_handler = new TextFileHandler("Assets/Resources/player_scores.csv");
+        button_obj = this.GetComponent<Button>();
+        button_obj.onClick.AddListener(SaveAndReset);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SaveAndReset()
+    {
+        // Capture current player_name and player_score, and write it to a new row the save_file CSV, along with the timestamp
+        // Afterwards, reset the game to the initial state (via the game controller).
+    }
+}
---
+++ Assets/Scripts/TextFileHandler.cs
@@ -0,0 +1,50 @@
+﻿// Based off of https://support.unity3d.com/hc/en-us/articles/115000341143-How-do-I-read-and-write-data-from-a-text-file-
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class TextFileHandler
+{
+    // The path of the text file to be used for reading or writing.
+    // This file path should be defined during instantiation instead of being hard-coded.
+    public string path;    
+    
+    public TextFileHandler(string file_path) {
+        this.path = file_path;
+    }
+
+    [MenuItem("Tools/Write file")]
+    static void WriteString()
+    {
+        //string path = "Assets/Resources/test.txt";
+
+        //Write some text to the test.txt file
+        StreamWriter writer = new StreamWriter(this.path, true);
+        writer.WriteLine("Test");
+        writer.Close();
+
+        //Re-import the file to update the reference in the editor
+        AssetDatabase.ImportAsset(this.path); 
+        TextAsset asset = Resources.Load("test");
+
+        //Print the text from the file
+        Debug.Log(asset.text);
+    }
+
+    [MenuItem("Tools/Read file")]
+    static void ReadString()
+    {
+        //string path = "Assets/Resources/test.txt";
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(this.path); 
+        Debug.Log(reader.ReadToEnd());
+        reader.Close();
+    }
+
+}
+
+//For more information, consult the following documentation:
+//    Unity TextAsset script reference documentation
+//    Microsoft StreamWriter class documentation
+//    Microsoft StreamReader class documentation
